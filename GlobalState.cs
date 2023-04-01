b77b2d91bb0e93c1f939f3377f76c7ac43d12fa5
ioan <ioan.macavei2001@gmail.com>
2023-04-01 20:11:13 +0300

    Auto stash before merge of "main" and "origin/main"

--- GlobalState.cs
+++ GlobalState.cs
@@ -5,6 +5,9 @@ namespace Awesomegame;
 public partial class GlobalState : Node2D
 {
 	private int _gold = 0;
+	private float _darkness;
+
+	public  bool Sane { get; set; }
 
 	public bool Sane { get; set; }
 
@@ -20,6 +23,27 @@ public partial class GlobalState : Node2D
 			_gold = value;
 			var label = GetTree().CurrentScene.FindChild("gold_l") as Label;
 			label.Text = Gold.ToString();
+		}
+	}
+
+	public float Darkness
+	{
+		get => _darkness;
+		set
+		{
+			_darkness = value;
+			if (value > 0.95f)
+				_darkness = 0.95f;
+			
+			foreach (player node in GetTree().GetNodesInGroup("players"))
+			{
+				//player.Set
+			}
+			
+			
+			
+			var light = GetTree().CurrentScene.FindChild("darkness") as DirectionalLight2D;
+			light.Energy = _darkness;
 		}
 	}
 }
---
+++ darkness.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+using Awesomegame;
+
+public partial class darkness : DirectionalLight2D
+{
+	private GlobalState state;
+
+	private double elapsed = 0;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		state = GetTree().CurrentScene.FindChild("globals") as GlobalState;
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		elapsed += delta;
+
+		if (elapsed >= 1)
+		{
+			state.Darkness += 0.1f;
+			elapsed = 0;
+		}
+		
+
+	}
+}
