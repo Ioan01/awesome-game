f5b66f46436048b9aaf73e4c405502932440d7b0
ioan <ioan.macavei2001@gmail.com>
2023-04-01 11:13:41 +0300

    zomni

--- main.cs
+++ main.cs
@@ -1,11 +1,29 @@
 using Godot;
 using System;
 
-public partial class main : Node2D
+public partial class root : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
+	private TileMap walls;
+
+	private NavigationRegion2D region2D;
+
+	public override void _EnterTree()
+	{
+		
+	}
+
 	public override void _Ready()
 	{
+		walls = FindChild("floor") as TileMap;
+
+		var rid = NavigationServer2D.MapCreate();
+		region2D = FindChild("nav") as NavigationRegion2D;
+		
+		NavigationServer2D.RegionSetMap(region2D.GetRegionRid(),rid);
+		
+
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ movement.cs
@@ -0,0 +1,76 @@
+using Godot;
+
+public partial class movement : CharacterBody2D
+{
+	private AnimatedSprite2D sprite2D = null!;
+
+	private CollisionShape2D collision = null!;
+
+	private bool isFlipped;
+
+	private bool wasFlipped;
+
+	private bool isFacingRight = true;
+	
+	
+	[Export]
+	public float Speed { get; set; }= 800.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public override void _Ready()
+	{
+		sprite2D = FindChild("animations") as AnimatedSprite2D;
+		collision = FindChild("collision") as CollisionShape2D;
+		
+		sprite2D.Play("idle");
+		
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+
+			if (direction.X < 0)
+			{
+				isFlipped = true;
+			}
+			else isFlipped = false;
+
+			if (isFlipped != wasFlipped)
+				Scale = new Vector2(-1 * Scale.X, Scale.Y);
+
+			
+			wasFlipped = isFlipped;
+			
+			
+			
+			sprite2D.Play("run");
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			
+			sprite2D.Play("idle");
+
+		}
+		
+
+		
+		
+		
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
