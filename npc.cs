e58575bb13f9caa41162eeb3a3ec1b1185e59104
Radu Micea <radu.micea@student.upt.ro>
2023-04-01 15:26:44 +0300

    merge

---
+++ npc.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+using System.Collections.Generic;
+using Awesomegame;
+
+public partial class npc : character
+{
+	[Export] protected override float Speed { get; set; } = 800;
+
+	public override void _Ready()
+	{
+		AddToGroup("npcs");
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		Move(GetTarget().Position);
+	}
+
+	protected virtual CharacterBody2D GetTarget()
+	{
+		return null!;
+	}
+}
