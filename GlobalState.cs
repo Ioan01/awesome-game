43c49be6e3e90c7db3b5009608cdb478bef69f09
Sebastian Niguleasa <seba80443@gmail.com>
2023-04-01 14:45:51 +0300

    Add line for debugging error

--- GlobalState.cs
+++ GlobalState.cs
@@ -1,8 +1,8 @@
-﻿namespace Awesomegame;
+namespace Awesomegame;
 
 public static class GlobalState
 {
-    public static bool Sane { get; set; }
+	public static bool Sane { get; set; }
 
-    public static float SpeedModifier { get; set; } = 1f;
+	public static float SpeedModifier { get; set; } = 1f;
 }
